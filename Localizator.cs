ffdc65eef80a9f664e398313e0db58404d1bcc28
atauenis <atauenis@ya.ru>
2013-11-23 22:27:40 +0400

    ветке fcmd-xwt возвращена функциональность предыдущих версий

--- Localizator.cs
+++ Localizator.cs
@@ -1,5 +1,6 @@
-﻿/* The File Commander shared (cross-platform) kernel
- * UI localizer     Модуль для работы с переводами интерфейса на разные языки
+﻿/* The File Commander backend   Ядро File Commander
+ * UI localizer                 Модуль для работы с переводами интерфейса на разные языки
+ * (C) The File Commander Team - https://github.com/atauenis/fcmd
  * (C) 2013, Alexander Tauenis (atauenis@yandex.ru)
  * Contributors should place own signs here.
  */
@@ -68,7 +69,7 @@ namespace fcmd
                         "FCVEdit=&Правка\nFCVEditCopy=&Копировать\nFCVEditSelAll=&Выделить всё\nFCVEditSearch=&Найти...\nFCVEditSearchNext=Искать дальше\n" +
                         "FCVView=&Вид\nFCVViewModeText=&Текст\nFCVViewModeImage=&Рисунок\n" +
                         "FCVFormat=&Формат\nFCVHelpMenu=&Справка\nFCVHelpAbout=&О программе и используемом модуле просмотра\n" +
-                        "FCVWhatFind=Введите искомую строку\nFCVNothingFound=Запрошенный текст в файле не найден";
+                        "FCVWhatFind=Введите искомую строку\nFCVNothingFound=Запрошенный текст в файле не найден\nFCVTxtCodepages=Кодовая страница";
         Dictionary<string, string> Localization = new Dictionary<string, string>();
         
         /// <summary>
