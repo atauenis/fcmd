a814a8347eb2afa8ae3124602cd5cdb7ec73dbb6
atauenis <atauenis@ya.ru>
2014-01-24 13:29:34 +0400

    странно, не все файлы закоммитились; исправляю

---
+++ pluginner/Utilities.cs
@@ -0,0 +1,50 @@
+﻿/* The File Commander - plugin API
+ * FC & FC plugin utility set
+ * (C) The File Commander Team - https://github.com/atauenis/fcmd
+ * (C) 2013-14, Alexander Tauenis (atauenis@yandex.ru)
+ * Contributors should place own signs here.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace pluginner
+{
+    class Utilities
+    {
+        /// <summary>Loads embedded resource</summary>
+        /// <param name="resourceName">The name of the resource</param>
+        /// <param name="assembly">The assembly, which contains the resource</param>
+        public static string GetEmbeddedResource(string resourceName, Assembly assembly)
+        {
+            resourceName = FormatResourceName(assembly, resourceName);
+            using (Stream resourceStream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (resourceStream == null)
+                    return null;
+
+                using (StreamReader reader = new StreamReader(resourceStream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        private static string FormatResourceName(Assembly assembly, string resourceName)
+        {
+            return assembly.GetName().Name + "." + resourceName.Replace(" ", "_")
+                                                               .Replace("\\", ".")
+                                                               .Replace("/", ".");
+        }
+
+        /// <summary>Loads embedded resource from current program</summary>
+        /// <param name="resourceName">The name of the resource</param>
+        public static string GetEmbeddedResource(string resourceName)
+        {
+            return GetEmbeddedResource(resourceName, Assembly.GetExecutingAssembly());
+        }
+    }
+}
